3329e7fa07cf6aed0ac20ad793c39f9a142256ba
ivy <Macjecy@Gmail.com>
2018-08-08 11:26:24 +0800

    Add comment and remove some redundancy of the file 'point_pair_with_path_in_a_tree.cpp'.

--- TheMethodofProgramming/Chapter03_Tree/point_pair_with_longest_path_in_a_tree.cpp
+++ TheMethodofProgramming/Chapter03_Tree/point_pair_with_longest_path_in_a_tree.cpp
@@ -7,6 +7,10 @@
 #define DEPTH 5
 using namespace std;
 
+/*
+Definition of node of an auxiliary tree to record the longest
+path of a given tree.
+*/
 struct TreePathNode
 {
     int depth_left, depth_right, len, come_from;
@@ -22,6 +26,9 @@ struct TreePathNode
 };
 
 
+/*
+Simple definition of a tree node to show the algorithm.
+*/
 struct TreeNode
 {
     int data;
@@ -33,22 +40,31 @@ struct TreeNode
     }
 };
 
-
-template <typename TreeNode>
-void free_tree(TreeNode**root, int mxDepth = 1)
+/*
+print the data field 'data' of a tree node.
+*/
+void print_node(TreeNode *node)
 {
-    // if(NULL == root || NULL == *root || mxDepth == 0)
-    if(NULL == root || NULL == *root)
-        return;
-    // cout << "about to deleting..." << endl;
-    free_tree(&((*root)->lchild), mxDepth-1);
-    free_tree(&((*root)->rchild),mxDepth-1);
-    delete(*root);
-    // cout << "deleted" << endl;
-    *root = NULL; // Never forget to do this.
+    cout << node->data << endl;
 }
 
+/*
+Destroy a tree recursively.
+*/
+template <typename TreeNode>
+void free_tree(TreeNode*&root)
+{
+    if(NULL == root)
+        return;
+    free_tree(root->lchild);
+    free_tree(root->rchild);
+    delete root;
+    root = NULL; // Never forget to do this.
+}
 
+/*
+print a node of TreePathNode.
+*/
 void print_tree_path_node(TreePathNode*node)
 {
     if(node)
@@ -57,6 +73,15 @@ void print_tree_path_node(TreePathNode*node)
 }
 
 
+/*
+Recursively print the nodes of a tree and its structure horizontally.
+Args
+root: root node of the tree;
+print_func: the func to print each node of tree;
+depth: current depth of the root;
+branch: a bool array to record the branches to print;
+item_width: output width of each node.
+*/
 template <typename TreeNode>
 void print_tree(TreeNode *root, void (*print_func)(TreeNode*),\
                  int depth, bool branch[], int item_width = 3)
@@ -65,23 +90,21 @@ void print_tree(TreeNode *root, void (*print_func)(TreeNode*),\
         return;
     for (int i = 0; i < depth; ++i)
     {
+        char twig;
+        if (i == depth - 1)
+            twig = '-';
+        else
+            twig = ' ';
+
         if (branch[i])
             printf("|");
-        else if (i == depth-1)
-            putchar('-');
         else
-            putchar(' ');
-        if (i == depth - 1)
-        {
-            for (int i = 0; i < item_width-1; ++i)
-                putchar('-');
-        }
-        else
-        {
-            for (int i = 0; i < item_width-1; ++i)
-                putchar(' ');
-        }
+            putchar(twig);
+
+        for (int i = 0; i < item_width; ++i)
+            putchar(twig);
     }
+
     print_func(root);
 
     if (root->rchild) // print the branch only when the tree has right children.
@@ -94,7 +117,13 @@ void print_tree(TreeNode *root, void (*print_func)(TreeNode*),\
     print_tree(root->rchild, print_func, depth+1, branch);
 }
 
-
+/*
+Randomly generate a binary tree with the specified depth.
+args
+depth: max depth of the tree to generate;
+cnt: counting of the node, used as the node data field;
+reach_bottom: a flag showing if the tree has reached the max depth.
+*/
 TreeNode* generate_rand_binary_tree(int depth, int &cnt, bool &reach_bottom)
 {
     if (depth < 0)
@@ -105,8 +134,7 @@ TreeNode* generate_rand_binary_tree(int depth, int &cnt, bool &reach_bottom)
     TreeNode *root = new TreeNode(cnt);
     // randomly have children when reach_bottom is true,
     // i.e. leaf nodes at the maximum depth have been generated.
-    bool have_children = !reach_bottom || rand() % 2;
-    if(have_children)
+    if(!reach_bottom || rand() % 2)
     {
         root->lchild = generate_rand_binary_tree(depth-1, cnt, reach_bottom);
         root->rchild = generate_rand_binary_tree(depth-1, cnt, reach_bottom);
@@ -116,7 +144,17 @@ TreeNode* generate_rand_binary_tree(int depth, int &cnt, bool &reach_bottom)
     return root;
 }
 
-
+/*
+Target func to show the algorithm how to calculate the longest path in a binary
+tree whose edges are treated as bidirectional.
+Args
+root: root of the tree;
+depth: current depth of the root.
+return
+a binary tree with each node showing the longest path of a corresponding subtree,
+and its structure is similar to the given tree, with some subtrees removed
+if they cannot form part of the longest path.
+*/
 template <typename TreeNode>
 TreePathNode* longest_path(TreeNode * root, int depth)
 {
@@ -125,8 +163,8 @@ TreePathNode* longest_path(TreeNode * root, int depth)
     TreePathNode *d, *dl, *dr;
     // printf("node,depth:%d, %d\n", root->data, depth);
     d  = new TreePathNode(0,0,0,0);
-    dl = longest_path(root->lchild,depth+1);
-    dr = longest_path(root->rchild,depth+1);
+    dl = longest_path(root->lchild, depth+1);
+    dr = longest_path(root->rchild, depth+1);
     if(dl)
         d->depth_left  = 1 + max(dl->depth_left, dl->depth_right);
     if(dr)
@@ -154,53 +192,48 @@ TreePathNode* longest_path(TreeNode * root, int depth)
     }
     switch(d->come_from)
     {
-         // the path is comprised of the max subpath from the lchild,
-         // the root and the max subpath from rchild.
+        // the path is comprised of the max subpath from the lchild,
+        // the root and the max subpath from rchild.
         case 1:
             // delete the grandchidren from his lchild.
             if (dl && d->lchild->depth_left > d->lchild->depth_right)
             {
-                free_tree(&(d->lchild->rchild));
+                free_tree(d->lchild->rchild);
                 assert(NULL == d->lchild->rchild);
             }
             else if (dl && d->lchild->depth_left < d->lchild->depth_right)
             {
-                free_tree(&(d->lchild->lchild));
+                free_tree(d->lchild->lchild);
                 assert(NULL == d->lchild->lchild);
             }
             // delete the grandchidren from his rchild.
             if (dr && d->rchild->depth_left > d->rchild->depth_right)
             {
-                free_tree(&(d->rchild->rchild));
+                free_tree(d->rchild->rchild);
                 assert(NULL == d->rchild->rchild);
             }
             else if (dr && d->rchild->depth_left < d->rchild->depth_right)
             {
-                free_tree(&(d->rchild->lchild));
+                free_tree(d->rchild->lchild);
                 assert(NULL == d->rchild->lchild);
             }
             break;
         // the max path is from lchild, so free delete rchild.
         case 2:
-            free_tree(&(d->rchild));
+            free_tree(d->rchild);
             break;
         // the max path is from rchild, so free delete lchild.
         case 4:
-            free_tree(&(d->lchild));
-        // default:
-        //     printf("ERROR! unknown come_from code: %d\n", d->come_from);
+            free_tree(d->lchild);
     }
     return d;
 }
 
 
-void  print_node(TreeNode *node)
-{
-    cout << node->data << endl;
-}
-
-
-void  test()
+/*
+Simply test the correctness of the algorithm.
+*/
+void test()
 {
     int cnt = 0;
     bool reached = false, branch[DEPTH] = {false};
@@ -212,7 +245,9 @@ void  test()
     TreePathNode * rootpath = longest_path(root,0);
     cout << "the result tree: [ldepth,rdepth,len,comefrom]" << endl;
     print_tree(rootpath, print_tree_path_node, 0, branch);
-    // free_tree(root);
+    free_tree(root);
+    free_tree(rootpath);
+    assert(root == NULL && rootpath == NULL);
 }
 
 
