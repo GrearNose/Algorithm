103ac93302aa94287c3609be09d3789bf89a5961
GrearNose <macjecy@Gmail.com>
2018-08-29 09:56:18 +0800

    Cpp code template of OJ.

---
+++ Basic/cpp_code_template.cpp
@@ -0,0 +1,37 @@
+// Cpp code template for OJ.
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#define INF 0x3f3f3f3f
+#define eps 1e-8
+#define MAXN 500000+10
+#define MAXM 50000000
+#define Ri(a) scanf("%d", &a)
+#define Rl(a) scanf("%lld", &a)
+#define Rs(a) scanf("%s", a)
+#define Pi(a) printf("%d\n", (a))
+#define Pl(a) printf("%lld\n", (a))
+#define Ps(a) printf("%s\n", (a))
+#define W(a) while(a--)
+#define CLR(a, b) memset(a, (b), sizeof(a))
+#define MOD 1000000007
+#define LL long long
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    while(EOF != Ri(n))
+    {
+        ;
+    }
+    return 0;
+}
